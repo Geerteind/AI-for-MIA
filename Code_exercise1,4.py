4dd64fd1ab382ca87c0543c1a3da4cd015d717c4
JellevNamen <j.p.v.namen@student.tue.nl>
2025-02-19 15:59:46 +0100

    small errors fixed

--- Code_exercise1,4.py
+++ Code_exercise1,4.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from PIL import Image
 
 def visualize_Images(directory, nr_samples=5):
-    classes = {"0": "Without Metastases", "1": "With Metastases"}     #train and validation map exists of a '0' and a '1' map
+    classes = {"0": "Without Metastasis", "1": "With Metastasis"}     #train and validation map exists of a '0' and a '1' map
     samples = {}	
     for Group in classes.keys():
         samples[Group] = get_sample_images(directory, Group, nr_samples)
@@ -12,7 +12,7 @@ def visualize_Images(directory, nr_samples=5):
 
 
     fig, axes = plt.subplots(2, nr_samples, figsize=(15, 6))          #create figure
-    fig.suptitle("Comparison of tissue with or without metastases", fontsize=14)
+    fig.suptitle("Comparison of tissue with or without metastasis", fontsize=14)
 
 
     for i, Group in enumerate(classes.keys()):
@@ -36,9 +36,9 @@ def get_sample_images(directory, Classnr, num_samples=5):
     return random.sample(image_files, min(num_samples, len(image_files)))               #take random samples from data
 
 
-# Run for training dataset
-visualize_Images("train")
+# Run for training dataset, change the directory when necessary.
+visualize_Images(r"path")
 
 
-# Run for validation dataset
-visualize_Images("valid")
+# Run for validation dataset, if images from validation dataset are wanted.
+#visualize_Images(r"path")
